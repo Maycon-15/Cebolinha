bd5950c195a8950631747bc8d9c1c0eea89f1033
Maycon-15 <78987309+Maycon-15@users.noreply.github.com>
2021-03-14 22:21:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+            string FraseDoCebolinha;
+            Console.WriteLine("Digite algo para que o Cebolinha possa repetir:");
+            frase = Console.ReadLine();
+
+            Console.WriteLine();
+
+            FraseDoCebolinha = frase.Replace("rr","l");
+            FraseDoCebolinha = frase.Replace("r","l");
+            
+
+            Console.WriteLine($"Cebolinha: {FraseDoCebolinha}");
+            
+        }
+    }
+}
